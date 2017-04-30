fc7f4d3e46ef241fb8dc060a806f34a4adcfa742
Austin Proctor <drproctor@fb.com>
2017-04-29 22:24:02 -0700

    Added  and fixed system method

--- git_pull_all.rb
+++ git_pull_all.rb
@@ -1,18 +1,20 @@
-# Git pull on each git repo directory
+# git pull on each git repo directory
+
+require 'colorize'
 
 # Add additional repos as needed.
 repos = [
   '~/GitHub/powershell',
   '~/GitHub/bash',
-  '~/GitHub/Ruby'
+  '~/GitHub/Ruby',
+  '~/GitHub/Collab_klborders/Ruby',
 ]
 
 repos.each do |r|
   begin
-    puts "Pulling the #{r} repo..."
-    system( "cd #{r}")
-    system( "git pull origin master" )
+    puts "Pulling the #{r} repo...\n".green
+    system( "cd #{r} && git pull origin master" )
   rescue
-    puts "Failed to pull #{r}."
+    puts "Failed to pull #{r}.".red
   end
 end
