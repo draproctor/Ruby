aafac9805ab18a856158b8b9ff39a7279ef9ee48
Austin Proctor <drproctor@fb.com>
2017-05-16 10:43:24 -0700

    clean up

--- screenshot_archiver.rb
+++ screenshot_archiver.rb
@@ -6,7 +6,7 @@ require 'fileutils'
 
 # Created an archived screenshots folder
 def archive_dir?
-  archive_folder = '/Users/'+@username+'/Documents/Archived Screenshots'
+  archive_folder = '/Users/' + @username + '/Documents/Archived Screenshots'
   unless Dir.exist?(archive_folder)
     Dir.mkdir(archive_folder)
   end
