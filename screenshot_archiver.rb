877e354bbe24f5547d9aef3668d4c692bd9a2339
Austin Proctor <drproctor@fb.com>
2017-05-17 08:27:23 -0700

    Replaced interpolation with concatenation

--- screenshot_archiver.rb
+++ screenshot_archiver.rb
@@ -5,19 +5,19 @@ require 'fileutils'
 
 # Created an archived screenshots folder
 def archive_dir?
-  archive_folder = '/Users/' + @username + '/Documents/Archived Screenshots'
+  archive_folder = '/Users/#{@username}/Documents/Archived Screenshots'
   Dir.mkdir(archive_folder) unless Dir.exist?(archive_folder)
   return archive_folder
 end
 
 # for each item in the screenshot dir, move to archived screenshots dir
 def move_captures
-  scrn_dir = '/Users/' + @username + '/Desktop'
+  scrn_dir = '/Users/#{@username}/Desktop'
   contents = Dir.entries(scrn_dir)
   contents.each do |file|
     if (File.extname(file).include? ".png") && (file.include? "Screen Shot")
       file_src = scrn_dir + '/' + file
-      puts 'Moving ' + file + ' to archived captures.'
+      puts 'Moving #{file} to archived captures.'
       FileUtils.mv(file_src, archive_dir?())
     end
   end
