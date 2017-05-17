50996a6e628dfd74cbb15b32c1de517039e688b8
Austin Proctor <drproctor@fb.com>
2017-05-17 12:35:27 -0700

    Added sub folder directory creation

--- screenshot_archiver.rb
+++ screenshot_archiver.rb
@@ -1,27 +1,38 @@
 # Organize all screenshots into folders
 require 'fileutils'
+require 'time'
 
 @username = ENV['USER']
 
 # Created an archived screenshots folder
 def archive_dir?
-  archive_folder = '/Users/#{@username}/Documents/Archived Screenshots'
+  archive_folder = "/Users/#{@username}/Documents/Archived Screenshots"
   Dir.mkdir(archive_folder) unless Dir.exist?(archive_folder)
   return archive_folder
 end
 
+# Organize all screenshots into folders for a specific day
+def sub_dir
+  time = Time.now
+  dir_name = "Archive #{time.month}-#{time.day}-#{time.year}"
+  sub_dir_name = "#{archive_dir?}/#{dir_name}"
+  Dir.mkdir(sub_dir_name) unless Dir.exist?(sub_dir_name)
+  return sub_dir_name
+end
+
 # for each item in the screenshot dir, move to archived screenshots dir
 def move_captures
-  scrn_dir = '/Users/#{@username}/Desktop'
+  scrn_dir = "/Users/#{@username}/Desktop"
   contents = Dir.entries(scrn_dir)
   contents.each do |file|
     if (File.extname(file).include? ".png") && (file.include? "Screen Shot")
       file_src = "#{scrn_dir}/#{file}"
-      puts 'Moving #{file} to archived captures.'
-      FileUtils.mv(file_src, archive_dir?())
+      puts "Moving #{file} to archived captures."
+      FileUtils.mv(file_src, sub_dir())
     end
   end
 end
 
 archive_dir?()
+sub_dir()
 move_captures()
