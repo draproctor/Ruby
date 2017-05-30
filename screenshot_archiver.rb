a62ac652d038d61e23b97fa500edff4dbae22607
Austin Proctor <drproctor@fb.com>
2017-05-30 14:25:41 -0700

    Replaced the archive_dir method returned the archive_folder with a global variable

--- screenshot_archiver.rb
+++ screenshot_archiver.rb
@@ -6,18 +6,20 @@ require 'time'
 
 # Log sorting is done by current time/date. Declaring this early saves space.
 @t = Time.now
+@archive_folder = "/Users/#{@username}/Documents/Archived Screenshots"
+
 
 # Created an archived screenshots folder
 def archive_dir
   archive_folder = "/Users/#{@username}/Documents/Archived Screenshots"
-  Dir.mkdir(archive_folder) unless Dir.exist?(archive_folder)
+  Dir.mkdir(@archive_folder) unless Dir.exist?(@archive_folder)
   return archive_folder
 end
 
 # Organize all screenshots into folders for a specific day
-def sub_dir(ad)
+def sub_dir
   dir_name = "Archive #{@t.month}-#{@t.day}-#{@t.year}"
-  sub_dir_name = "#{ad}/#{dir_name}"
+  sub_dir_name = "#{@archive_folder}/#{dir_name}"
 
   Dir.mkdir(sub_dir_name) unless Dir.exist?(sub_dir_name)
   return sub_dir_name
@@ -43,9 +45,9 @@ end
 
 # Make a hiddle folder for saving the archive logs and edit the log to include
 # number of items moved, what day, and at what time.
-def mk_log(ad, mc)
+def mk_log(mc)
   log_name = "#{@t.month}/#{@t.day}/#{@t.year} at #{@t.hour}:#{@t.min}"
-  log_dir = "#{ad}/.archive_logs"
+  log_dir = "#{@archive_folder}/.archive_logs"
   Dir.mkdir(log_dir) unless Dir.exist?(log_dir)
 
   log_file = "#{log_dir}/Archive Log.txt"
@@ -56,5 +58,5 @@ def mk_log(ad, mc)
   end
 end
 
-sub_dir(archive_dir())
-mk_log(archive_dir(),move_captures())
+sub_dir()
+mk_log(move_captures())
